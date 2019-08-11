02a7ce65e6423acb5bbd7a269aa814f7751c68f6
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-11 01:14:20 -0400

    Added search route and font awesome script

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <div id="app">hello</div>
+  <div class="container">
+    <Header />
+    <router-view />
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header";
+
 export default {
   name: "app",
-  components: {}
+  components: {
+    Header
+  }
 };
 </script>
 
@@ -13,46 +20,49 @@ export default {
 :root {
   --primary-color: #953036;
 }
+
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
+
 body {
   background: var(--primary-color);
   font-family: Arial, Helvetica, sans-serif;
   color: #fff;
   line-height: 1.6;
 }
+
 a {
   color: #fff;
   text-decoration: none;
 }
+
 ul {
   list-style: none;
 }
+
 .body-bg-no-image {
   background: var(--primary-color);
 }
+
 .body-bg-image {
   background: var(--primary-color) url("./assets/octane.png") no-repeat top
     center;
 }
+
 .container {
   max-width: 960px;
   margin: 1rem auto;
   overflow: auto;
   padding: 0 2rem;
 }
+
 .form-group {
   margin: 1rem 0;
 }
-.search {
-  background: rgba(0, 0, 0, 0.5);
-  border: 4px #fff solid;
-  margin-top: 1rem;
-  padding: 2rem;
-}
+
 input,
 select,
 textarea {
@@ -62,6 +72,7 @@ textarea {
   font-size: 1.2rem;
   border: 1px solid #ccc;
 }
+
 .btn {
   display: inline-block;
   background: var(--primary-color);
@@ -72,6 +83,7 @@ textarea {
   outline: none;
   margin-top: 1rem;
 }
+
 @media (max-width: 700px) {
   body {
     background: var(--primary-color);
